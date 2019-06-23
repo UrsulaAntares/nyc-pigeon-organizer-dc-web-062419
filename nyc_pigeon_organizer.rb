6c200bdc5a2785c814af2a6709d6aa689a408ce3
Ursula Miller <ursula.miller@gmail.com>
2019-06-23 17:47:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,10 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   whos_who = []
   data.each do |attribute, data|
-    data.each do |specific, name|
-    whos_who << name
-  binding.pry
+      data.each do |specific, name|
+      whos_who << name
+    binding.pry
+    end
   end
   binding.pry
 end
