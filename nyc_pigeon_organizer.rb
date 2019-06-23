f50841408911940bcff0e4122843fe577bacc9b6
Ursula Miller <ursula.miller@gmail.com>
2019-06-23 18:15:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   whos_who = []
+  attributes = []
   data.each do |attribute, data_sub|
       data_sub.each do |specific, name_set|
         name_set.each do |name|
@@ -14,9 +15,12 @@ def nyc_pigeon_organizer(data)
   #          pigeon_list[name][attribute] << specific.to_s
   #     binding.pry
   #        end  
+          if attributes.include?(attribute) != true
+            attributes << attribute 
+          end
         end
         name_set.each do |name|
-          pigeon_list[name][attribute] = []
+ #         pigeon_list[name][attribute] = []
         end
  #   binding.pry
     end
